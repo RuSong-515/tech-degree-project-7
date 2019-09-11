c9228a737e62cea7d40d315d33356e99fde6269b
Ru Song <rusong.usc@gmail.com>
2019-09-10 23:23:14 -0700

    updating code

---
+++ js/messaging.js
@@ -0,0 +1,16 @@
+const user = document.getElementById('userField');
+const message = document.getElementById('messageField');
+const send = document.getElementById('send');
+
+send.addEventListener('click', () => {
+  if (message.value === '' && user.value === '') {
+    window.alert("User's field and message are both required.");
+  } else if (user.value === '') {
+    window.alert("User's field is required.")
+  } else if (message.value === '') {
+    window.alert("Message field is required.")
+  } else {
+    window.alert(`Your message has been sent to: ${user.value}`);
+  }
+});
+
